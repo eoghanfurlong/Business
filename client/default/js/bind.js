bf0ad436ffdd19bca582ffb0ec81ea3986fc79eb
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-22 17:52:04 +0000

    v2

--- client/default/js/bind.js
+++ client/default/js/bind.js
@@ -4,18 +4,6 @@
  */
 
 function bindEvents(){
-	$("#submitZendesk").bind("click",function(){
-		var subject=$("#subject").val();
-		var description=$("#des").val();
-		var email=$("#email").val();
-		
-		zendesk.newRequest(subject,description,email);
-	});
-	
-	$("#getRequests").bind("click",function(){
-		var email=$("#emailCheck").val();
-		zendesk.listUserRequests(email);
-	});
 	
 	$("#submitMortgage").bind("click",function(){
 		var years=$("#years").val();
@@ -26,24 +14,6 @@ function bindEvents(){
 		
 		mortgage.getMortgage(years,interest,loanAmount,tax,insurance);
 	});
-	
-	$("#submitStock").bind("click",function(){
-		var companyName=$("#companyName").val();
-		stock.getStockInfo(companyName);
-	});
-	
-	$("#nav_stock").bind("click",function(){
-		changePage("stock");
-	});
-	
-	$("#nav_mortgage").bind("click",function(){
-		changePage("mortgage");
-	});
-	
-	$("#nav_support").bind("click",function(){
-		changePage("zendesk");
-	});
-	changePage("stock");
 }
 
 
