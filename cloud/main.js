9d8c89f2094f028fe8c3a456f07ae2616898bf87
keyang xiang <keyang.xiang@feedhenry.com>
2012-02-23 15:12:34 +0000

    v3

--- cloud/main.js
+++ cloud/main.js
@@ -3,8 +3,23 @@
  */
 
 
+/**
+ * Create a new Request ticket in Zendesk with requester
+ */
+function newRequest(param){
+	return zendesk.newRequest(param.subject,param.des,param.email);
+}
 
+/**
+ * Retrieve all request tickets
+ */
+function listUserRequests(param){
+	return zendesk.listUserRequests(param.email);
+}
 
+/**
+ * Calculate mortgage with user input
+ */
 function getMortgage(param){
 	return mortgage.getMortgage(param.years,param.interest,param.loanAmount,param.tax,param.insurance);
 }
